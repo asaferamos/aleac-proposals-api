0cf21ca6d1d01d4b3e5cf1c3b714779a9e82da14
Asafe Ramos <asafe@asaferamos.com>
2019-03-08 11:17:05 -0300

    add bcrypt on password users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_180108) do
+ActiveRecord::Schema.define(version: 2019_03_07_181554) do
+
+  create_table "bills", force: :cascade do |t|
+    t.integer "ext_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bills_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
