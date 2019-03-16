38ef7bf29efac8e50bbcb13f18dd398adfe956e8
Asafe Ramos <asafe@asaferamos.com>
2019-03-15 22:04:47 -0300

    add proposal favorite and remove

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -3,6 +3,46 @@ require 'date'
 
 class BillsController < ApplicationController
     before_action :authorize_request
+
+    def index
+        @current_user = getUserActive()
+
+        proposals = Bill.where(user_id: @current_user.id)
+        render json: proposals
+    end
+
+    def create
+        @current_user = getUserActive()
+
+        proposal = Bill.new({
+            ext_id:  params[:ext_id],
+            user_id: @current_user.id
+        })
+
+        # test if proposal already salved before
+        if Bill.exists?({user_id: @current_user.id,ext_id: params[:ext_id]})
+            render json: ['already saved']
+        elsif proposal.save
+            render json: proposal.id, status: 200
+        else
+            render json: proposal.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        begin
+            proposal = Bill.where({user_id: @current_user.id,ext_id: params[:ext_id]}).take
+            if proposal
+                proposal.destroy
+                render json: true
+            else
+                render json: false, status: 400
+            end
+        rescue ActiveRecord::RecordNotFound => e
+            render json: false
+        end
+    end
+
     def new
         @ext_id = params[:ext_id]
 
@@ -130,5 +170,13 @@ class BillsController < ApplicationController
         else
             return location[1]
         end
+    end
+
+    def getUserActive()
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        decoded = JsonWebToken.decode(header)
+
+        return User.find(decoded[:user_id])
     end
 end
--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,3 +1,3 @@
 class Bill < ApplicationRecord
-    belongs_to :users
+    # belongs_to :users
 end
